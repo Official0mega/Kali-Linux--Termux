612ff0dc9799573f2606b8e1ba5edcd81f3a9776
David James Taiye <122806822+Official0mega@users.noreply.github.com>
2023-04-14 19:42:50 +0100

    Create Steps_to_Install_Kali_Termux.py

---
+++ Steps_to_Install_Kali_Termux.py
@@ -0,0 +1,25 @@
+
+Step1. Install termux with this link = https://f-droid.org/en/packages/com.termux/
+
+
+Step2. 
+* Open Termux and run the following commands:
+  ```
+  apt update && apt install python python2 openssh -y
+
+
+
+
+
+
+
+
+
+
+
+Below is the Link to get more info about Securing your Kali and other info.
+
+
+* https://www.makeuseof.com/how-to-change-root-password-kali-linux/
+
+* https://www.codelivly.com/termux-tutorial-with-complete-termux-commands/ 
